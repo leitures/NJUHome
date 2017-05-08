5a2d079c3fb59bfdf1c8bcd171c6661b7a6861f7
刘磊 <kira.36@live.cn>
2017-05-08 10:03:52 +0800

    version 2.1

--- NJUHome/js/roomPageBTemplate.js
+++ NJUHome/js/roomPageBTemplate.js
@@ -5,7 +5,9 @@
    '<ul class="ui-list ui-list-one ui-list-link ui-border-tb">'+
    '<li v-for="item in items" class="ui-border-t">'+
    '<div class="ui-list-thumb">'+
-   '<span style="background-image:url(\'../img/icon.png\')"></span>'+
+  //  '<span style="background-image:url(\'../img/icon.png\')"></span>'+
+   '<span v-bind:style="\'background-image:url(../img/\'+item[0].tags+\'.png)\'">'+
+   '</span>'+
    '</div>'+
    '<div class="ui-list-info">'+
    '<h4 class="ui-nowrap" style="font-size:0.267rem">{{item[0].title}}</h4>'+
