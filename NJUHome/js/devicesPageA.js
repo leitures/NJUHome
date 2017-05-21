dfe328e90dba52a32fd6d860dffe6636e1d8164b
刘磊 <kira.36@live.cn>
2017-05-21 15:52:39 +0800

    doc
     doc

--- NJUHome/js/devicesPageA.js
+++ NJUHome/js/devicesPageA.js
@@ -1,29 +1,3 @@
-// $.ajax({
-//         type: 'GET',
-//         url: 'http://joymebook.oss-cn-shanghai.aliyuncs.com/1.txt',
-//         headers: {
-//           // 'U-ApiKey': '6441e70eefc58fea0b1e938abf946a28',
-//           // 'U-ApiKey2': '222'
-//         },
-//
-//         dataType: 'json',
-//         success: function(res) {
-//             console.log(res);
-//         }
-//     });
-
-// $.ajax({
-//   url: 'http://api.yeelink.net/v1.0/devices',
-//   headers: {
-//     Accept: "application/json; charset=utf-8",
-//     'U-ApiKey': '6441e70eefc58fea0b1e938abf946a28'
-//   },
-//   type: "get",
-//   success: function(data) {
-//
-//   }
-// });
-
 var detaila = new Vue({
   el: '#data-typea-body',
   data: {
